691cfaa2ec549e82b793444b4dd5c376dd0ac11c
Egor Krivov <egor.krivov@frtk.ru>
2018-11-20 17:16:26 +0300

    Update run.py

--- simple_mnist/run.py
+++ simple_mnist/run.py
@@ -4,12 +4,6 @@ import functools
 
 from resource_manager import read_config
 
-#MODULES_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-#SHORTCUTS = {
-#    'dpipe_configs': os.path.join(MODULES_FOLDER, os.pardir, 'dpipe_configs'),
-#}
-
-# get_resource_manager = functools.partial(read_config, shortcuts=SHORTCUTS)
 
 if __name__ == "__main__":
 
